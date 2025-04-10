d99ee8155e53d13b72dafc8a78233367acb7018c
Harry Touloupas <harryt@realtwin.org>
2025-04-10 12:02:46 +0300

    feat: docker integration
    Signed-off-by: Harry Touloupas <harryt@realtwin.org>

--- run_rl_swarm.sh
+++ run_rl_swarm.sh
@@ -115,7 +115,6 @@ trap cleanup INT TERM
 
 # Default GPU_ID is 0 unless specified as first argument
 GPU_ID="${1:-0}"
-
 echo ">>> Running script for GPU=$GPU_ID ..."
 
 # 1) Set up environment for this GPU

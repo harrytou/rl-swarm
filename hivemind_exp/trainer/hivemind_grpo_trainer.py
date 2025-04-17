16d0ff2ed659b944ba9325f0d2deac3f0e75e4cf
Harry Touloupas <harryt@realtwin.org>
2025-04-17 18:14:11 +0300

    feat: 100x the rewards we are sending to DHT after stage 1
    Signed-off-by: Harry Touloupas <harryt@realtwin.org>

--- hivemind_exp/trainer/hivemind_grpo_trainer.py
+++ hivemind_exp/trainer/hivemind_grpo_trainer.py
@@ -89,8 +89,12 @@ class HivemindGRPOTrainer:
                     self.node.round_num, self.node.stage_num, q_hash, value
                 )
 
-                # Just the latest.
-                self.stage_rewards += sum(self.node.rewards)
+                if self.node.stage_num == 1:
+                    self.stage_rewards += abs(sum(self.node.rewards) * 100)
+                    self.logger.info(f"Stage 1 rewards (x100): {self.stage_rewards}")
+                else:
+                    self.stage_rewards += sum(self.node.rewards)
+                self.logger.info(f"Storing Stage {self.node.stage_num} rewards to DHT: {self.stage_rewards}")
                 self.dht.store(
                     key=rewards_key(self.node.round_num, self.node.stage_num),
                     subkey=self.node.key,
